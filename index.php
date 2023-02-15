b953f834fba49a1d9150fbe4066147533fcfa8a3
Josiah Carpenter <cheetahmoo@gmail.com>
2023-02-15 15:25:55 -0500

    First Push

---
+++ index.php
@@ -0,0 +1,21 @@
+    
+    
+    <?php
+    include("header.php")
+    ?>
+
+    <div class="intro-body welcom">
+        <div class="intro-text">
+            <h1>Hi, I'm Josiah</h1>
+            <p>A student at Cedarville University working on my bachelor's degree in Computer Science.</p>
+        </div>
+        <img class="intro-img" src="/assets/images/837427867561.png" alt="arrow">
+    </div>
+    <img class="bobbing" src="/assets/images/down_arrow.svg" alt="arrow" width="64">
+    <?php
+    include("footer.php")
+    ?>
+
+    </body>
+
+</html>
