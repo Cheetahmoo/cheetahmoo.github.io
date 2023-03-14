236166a7eefb0376c82b4c707e7e48f6df8b2e72
Josiah Carpenter <cheetahmoo@gmail.com>
2023-03-14 09:09:49 -0400

    Restructure
    Convert from PHP to html for static site

--- header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title>Josiah Carpenter</title>
-    <link rel="stylesheet" href="/css/style.css">
-    <script src="node_modules/jquery/dist/jquery.min.js"></script>
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
-</head>
