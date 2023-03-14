05d01275e10d2c7b18103703f1815e350d1976df
Josiah Carpenter <cheetahmoo@gmail.com>
2023-03-14 08:58:42 -0400

    Styling Tweaks
    - Added project screen shots
    - Added Project svg icon
    - removed unused images
    - Changed Some styling

--- header.php
+++ header.php
@@ -7,16 +7,3 @@
     <script src="node_modules/jquery/dist/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
 </head>
-<body>
-    <header class="cntr-container">
-        <h1><span style="color:cornflowerblue">&#60/&#62</span> Josiah Carpenter</h1>
-        <nav>
-            <a href="/projects">Projects</a>
-            <a href="/services">Services</a>
-            <a href="/process">Process</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
-            <a href="/blog">Blog</a>
-        </nav>
-    </header>
-    <main class="cntr-container">
--- index.php
+++ index.php
@@ -1,21 +1,57 @@
-    
-    
     <?php
     include("header.php")
     ?>
 
-    <div class="intro-body welcom">
-        <div class="intro-text">
-            <h1>Hi, I'm Josiah</h1>
-            <p>A student at Cedarville University working on my bachelor's degree in Computer Science.</p>
-        </div>
-        <img class="intro-img" src="/assets/images/837427867561.png" alt="arrow">
-    </div>
-    <img class="bobbing" src="/assets/images/down_arrow.svg" alt="arrow" width="64">
-    <?php
-    include("footer.php")
-    ?>
+    <body>
+        <nav class="side-nav">
+            <div id="profileBubble" class="profile">
+                <img src="/assets/images/profile_pic.png">
+            </div>
+            <h1><span style="color:cornflowerblue">&#60/&#62</span> Josiah Carpenter</h1>
+            <div class="nav-body">
+                <a href="/projects">Projects</a>
+                <a href="/services">Services</a>
+                <a href="/process">Process</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+                <a href="/blog">Blog</a>
+            </div>
+        </nav>
+        <main>
+            <section id="welcome">
+                <div class="intro-body welcom">
+                    <div class="intro-text">
+                        <h1>Hi, I'm Josiah</h1>
+                        <p>A student at Cedarville University working on my bachelor's degree in Computer Science.</p>
+                    </div>
+                    <img class="intro-img" src="/assets/images/837427867561.png" alt="arrow">
+                </div>
+                <img style="position: absolute;bottom: 0;" class="bobbing" src="/assets/images/down_arrow.svg" alt="arrow" width="64">
+            </section>
+            <section id="E-bit" style="background-image: url('assets/images/E-bit snapShot 0.1.2.JPG')">
+                <div class="intro-body welcom">
+                    <div class="intro-text">
+                        <h1>Hi, I'm Josiah</h1>
+                        <p>A student at Cedarville University working on my bachelor's degree in Computer Science.</p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div class="intro-body welcom">
+                    <div class="intro-text">
+                        <h1>Hi, I'm Josiah</h1>
+                        <p>A student at Cedarville University working on my bachelor's degree in Computer Science.</p>
+                    </div>
+                    <img class="intro-img" src="/assets/images/Screenshot_20230225_080641.png" alt="arrow">
+                </div>
+            </section>
+        </main>
+
+
+        <?php
+        include("footer.php")
+        ?>
 
     </body>
 
-</html>
+    </html>
