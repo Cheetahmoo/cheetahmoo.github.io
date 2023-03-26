d3c1f514636ccb579faea94cb7bb9cb334381597
Josiah Carpenter <cheetahmoo@gmail.com>
2023-03-26 10:51:22 -0400

    Completed Section Drafted
    Completed the main layout for all the sections and the content in them.
    
    The wording still need to be worked on but most of the main content and layout is finished

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
+function create_panel_html(img_src, title, disc) {
+    const panel = document.createElement('div');
+    panel.className = 'fullscreen-panel';
+    panel.innerHTML = `
+      <div class="colms-2">
+        <button id="close" type="button" onclick="close_panel()">&#10006;</button>
+        <div style="align-self: center;">
+          <img class="shadow-l" src="${img_src}" />
+        </div>
+        <section>
+          <h2>${title}</h2>
+          <hr />
+          <p>${disc}</p>
+        </section>
+      </div>`;
+    return panel;
+}
 
-
-function scollToElemnt(elementId){
-    var element = document.getElementById(elementId);
+function scroll_to_element(elementId) {
+    const element = document.getElementById(elementId);
     element.scrollIntoView(true);
 }
+
+function close_panel() {
+    $('.fullscreen-panel').attr('style', '');
+    document.body.style = '';
+}
+
+document.addEventListener('keydown', (event) => {
+    const code = event.code;
+    if (code === 'Escape') {
+        close_panel();
+    }
+}, false);
+
+$(function () {
+    //Add functionality to display panel on click
+    $('div.card').on('click', function (e) {
+        e.preventDefault();
+        document.body.style.overflow = 'hidden';
+        const card = $(this);
+        const img_src = card.find('img').attr('src');
+        const title = card.find('h4').text();
+        //Combined the both the
+        const disc = card.find('.card-desc').html();
+        const panel = $('.fullscreen-panel');
+        panel.find('.fullscreen-title').text(title);
+        panel.find('.fullscreen-description').html(disc);
+        panel.find('.fullscreen-img').attr('src', img_src);
+        panel.attr('style', 'visibility: visible; opacity: 1;');
+    });
+});
---
+++ assets/images/temp.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {!Array} */
+var _0x104a = ["toString", "slice", "validate", "reverse", "join", "cmx4dQ==", "wertyujkeqwkegzjxhgci", "0xF1", "charCodeAt"];
+(function(data, i) {
+  /**
+   * @param {number} selected_image
+   * @return {undefined}
+   */
+  var validateGroupedContexts = function fn(selected_image) {
+    for (; --selected_image;) {
+      data["push"](data["shift"]());
+    }
+  };
+  validateGroupedContexts(++i);
+})(_0x104a, 237);
+/**
+ * @param {string} ballNumber
+ * @param {?} opt_target
+ * @return {?}
+ */
+var _0x4109 = function PocketDropEvent(ballNumber, opt_target) {
+  /** @type {number} */
+  ballNumber = ballNumber - 0;
+  var ball = _0x104a[ballNumber];
+  return ball;
+};
+/**
+ * @param {?} args
+ * @return {?}
+ */
+var checke = function startup(args) {
+  /**
+   * @param {!Object} d
+   * @return {?}
+   */
+  var _0x238933 = function match(d) {
+    return d["split"]("")[_0x4109("0x0")]()[_0x4109("0x1")]("");
+  };
+  var importSave = _0x4109("0x2");
+  var _0x1c6caf = _0x4109("0x3");
+  var _0xab39c9 = _0x4109("0x4");
+  var API = {
+    "validate" : function send(event_data) {
+        console.log(_0xab39c9[_0x4109("0x5")](0)[_0x4109("0x6")](16) + 7 + atob(importSave) + _0x1c6caf[_0x4109("0x7")](3, 7));
+      return event_data === _0xab39c9[_0x4109("0x5")](0)[_0x4109("0x6")](16) + 7 + atob(importSave) + _0x1c6caf[_0x4109("0x7")](3, 7);
+    }
+  };
+  return API[_0x4109("0x8")](args);
+};
